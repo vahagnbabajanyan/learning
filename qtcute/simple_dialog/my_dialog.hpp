3ad68d1e066b8ae139ece2e5b0add2f4d808e967
vahagnbabajanyan <vahagndrive6@gmail.com>
2014-08-28 12:29:42 -0700

    first commit

---
+++ qtcute/simple_dialog/my_dialog.hpp
@@ -0,0 +1,15 @@
+#ifndef _MY_DIALOG_HPP_
+#define _MY_DIALOG_HPP_
+
+#include <QDialog>
+
+class QLabel;
+class QCheckBox;
+class QPushButton;
+class QLineEdit;
+
+class my_dialog : QDialog
+{
+};
+
+#endif
